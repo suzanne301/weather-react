9f0ddd0808ac0945b2c99bbc708a35eb61502f52
suzanne301 <104364608+suzanne301@users.noreply.github.com>
2024-07-02 00:58:31 +0100

    removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        React Weather
       </header>
+      <footer>
+        <p>
+          This project was coded by < a href = "https://www.linkedin.com/in/suzanne-paterson-778b492b9/"
+          target = "_blank" >Suzanne Paterson</a> {""}
+           and is <a> open-sourced on GitHub</a>
+        </p>
+      </footer>
     </div>
   );
 }
-
-export default App;
