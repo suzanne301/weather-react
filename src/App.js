4bd2a96480a9ebba6205beaf16230be4f7a35567
suzanne301 <104364608+suzanne301@users.noreply.github.com>
2024-07-02 01:04:56 +0100

    added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        React Weather
+        <h1> React Weather </h1>
       </header>
       <footer>
         <p>
           This project was coded by < a href = "https://www.linkedin.com/in/suzanne-paterson-778b492b9/"
           target = "_blank" >Suzanne Paterson</a> {""}
-           and is <a> open-sourced on GitHub</a>
+           and is < a href="https://github.com/suzanne301/weather-react" target="_blank"> open-sourced on GitHub < /a>
         </p>
       </footer>
     </div>
