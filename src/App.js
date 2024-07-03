138fb7526e40e823c3e65e332897c4c1ba912f52
suzanne301 <104364608+suzanne301@users.noreply.github.com>
2024-07-03 01:57:07 +0100

    added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> React Weather </h1>
-      </header>
-      <footer>
-        <p>
-          This project was coded by <a href="https://www.linkedin.com/in/suzanne-paterson-778b492b9/"
-          target="_blank" rel="noreferrer" > Suzanne Paterson </ a> {""}
-           and is < a href = "https://github.com/suzanne301/weather-react"
-           target="_blank" rel="noreferrer"> open-sourced on GitHub</a>
-        </p>
-      </footer>
+      <div className='container'>
+        <header className="App-header">
+          <h1> React Weather </h1> 
+        </header> 
+        <footer>
+          <p> This project was coded by <a href="https://www.linkedin.com/in/suzanne-paterson-778b492b9/" target="_blank" rel="noreferrer"> Suzanne Paterson </a> {""} and is <a href = "https://github.com/suzanne301/weather-react" target="_blank" rel="noreferrer"> open-sourced on GitHub </a> </p> 
+        </footer>
+      </div>
     </div>
   );
 }
