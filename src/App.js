fba47513db5c4a71773770c445a5be3052dc660a
suzanne301 <104364608+suzanne301@users.noreply.github.com>
2024-07-05 20:02:27 +0100

    added basic styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
+import React from 'react';
 import './App.css';
+import Weather from './Weather';
 
 export default function App() {
   return (
     <div className="App">
       <div className='container'>
         <header className="App-header">
-          <h1> React Weather </h1> 
         </header> 
+        <Weather />
         <footer>
-          <p> This project was coded by <a href="https://www.linkedin.com/in/suzanne-paterson-778b492b9/" target="_blank" rel="noreferrer"> Suzanne Paterson </a> {""} and is <a href = "https://github.com/suzanne301/weather-react" target="_blank" rel="noreferrer"> open-sourced on GitHub </a> </p> 
+            This project was coded by {" "}
+            <a 
+            href="https://www.linkedin.com/in/suzanne-paterson-778b492b9/" 
+            target="_blank" 
+            rel="noreferrer"
+            > 
+            Suzanne Paterson 
+            </a> {""} 
+            and is {" "}
+            <a 
+            href="https://github.com/suzanne301/weather-react" target="_blank" 
+            rel="noreferrer"
+            >
+             open-sourced on GitHub 
+            </a>
         </footer>
       </div>
     </div>
